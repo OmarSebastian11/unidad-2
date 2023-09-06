b38fee342ba187c3f44929c1949b7d1db38629eb
OmarSebastian11 <143815501+OmarSebastian11@users.noreply.github.com>
2023-09-06 08:46:47 -0600

    Add files via upload

---
+++ ListaTareas.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package listatareas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ListaTareas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String tarea1 ="hacer mi cama,";
+        String tarea2 ="hacer mi cuerto,";
+        String tarea3 ="lavar trastes,";
+        String tarea4 ="hacer la comida,";
+        String tarea5 ="hacer  mi tarea,";
+        
+        System.out.println("mis tareas son" + tarea1 + tarea2 + tarea3+ tarea4 + tarea5);
+    }
+    
+}
